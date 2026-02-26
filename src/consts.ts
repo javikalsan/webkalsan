4d15cd1d6ed140a7999a8b074e3c344c452bd882
javikalsan <1070397+javikalsan@users.noreply.github.com>
2026-02-26 19:55:39 +0100

    feat: change website consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "sorginorratz";
+export const SITE_TITLE = "ᖭ༏ᖫ sorginorratz ᖭ༏ᖫ";
 export const SITE_DESCRIPTION = "temporarily misanthropic";
